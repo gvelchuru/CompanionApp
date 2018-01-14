53ce9b15877f4df920b1f147a680b279e8c19419
SamSteinbock <samstein@uw.edu>
2018-01-13 18:51:27 -0800

    Server initialize

--- src/server.js
+++ src/server.js
@@ -1,1 +1,24 @@
+var firebase = require("firebase");
 
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAjf0slvgGxRlI5wujzu8Nsp-_SJgsmsb4",
+    authDomain: "companion-app-e1b98.firebaseapp.com",
+    databaseURL: "https://companion-app-e1b98.firebaseio.com",
+    storageBucket: "companion-app-e1b98.appspot.com",
+};
+firebase.initializeApp(config);
+
+var db = firebase.firestore();
+
+/****************************************************************************************/
+// // Initialize Firebase
+// var config = {
+//     apiKey: "AIzaSyAjf0slvgGxRlI5wujzu8Nsp-_SJgsmsb4",
+//     authDomain: "companion-app-e1b98.firebaseapp.com",
+//     databaseURL: "https://companion-app-e1b98.firebaseio.com",
+//     projectId: "companion-app-e1b98",
+//     storageBucket: "companion-app-e1b98.appspot.com",
+//     messagingSenderId: "843176657895"
+// };
+// firebase.initializeApp(config);
