7b1c33fd9ab346cc46852d19459e752670b6c195
SamSteinbock <samstein@uw.edu>
2018-01-13 20:14:06 -0800

    Server set up

--- src/server.js
+++ src/server.js
@@ -1,19 +1,23 @@
 var firebase = require("firebase");
 require('firebase/firestore');
 const express = require('express');
-var epp = express();
+var app = express();
 
 // Initialize Firebase
 var config = {
     apiKey: "AIzaSyAjf0slvgGxRlI5wujzu8Nsp-_SJgsmsb4",
     authDomain: "companion-app-e1b98.firebaseapp.com",
     databaseURL: "https://companion-app-e1b98.firebaseio.com",
+    projectId: "companion-app-e1b98",
     storageBucket: "companion-app-e1b98.appspot.com",
+    messagingSenderId: "843176657895"
 };
 firebase.initializeApp(config);
 
+// database reference point
 var db = firebase.firestore();
 
+// Adds a user to the database
 function addUser(name, location, destination, time) {
     var docRef = db.collection('users').doc(name);
 
@@ -24,13 +28,14 @@ function addUser(name, location, destination, time) {
     });
 }
 
+// Gets the array of
 function getClosestUsersTo(name) {
     var doc = db.collection('users').doc(name).collection('closestUsers').get();
     var closestUsers = doc.toJSON();
     return closestUsers;
 }
 
-app.listen(3000, () => console.log('Example app listening on port 3000'))
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
 /****************************************************************************************/
 // // Initialize Firebase
 // var config = {
